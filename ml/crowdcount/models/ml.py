e19ad1e7d738fa2fef00c38bc1054a8a5abfb240
Dimitri Roche <dimroc@gmail.com>
2017-09-07 15:39:05 -0400

    Add keras instead of tf

--- ml/crowdcount/models/ml.py
+++ ml/crowdcount/models/ml.py
@@ -1,15 +1,19 @@
-from __future__ import print_function
-import tensorflow as tf
+from keras.layers import Dense, Activation
+from keras.models import Sequential
+import numpy as np
 
 
 def train():
-    hello = tf.constant('Hello, TensorFlow!')
+    model = Sequential()
+    model.add(Dense(units=5, input_dim=5))
+    model.add(Activation('relu'))
+    model.add(Dense(units=2))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
-    # Start tf session
-    sess = tf.Session()
-
-    # Run the op
-    print(sess.run(hello))
+    # x_train and y_train are Numpy arrays --just like in the Scikit-Learn API.
+    model.fit(np.random.rand(5, 5), np.random.rand(5, 2), epochs=5, batch_size=32)
+    print(model.evaluate(np.random.rand(1, 5), np.random.rand(1, 2), batch_size=128))
 
 
 def test():
