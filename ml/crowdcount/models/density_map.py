cf5c08ea5dff7bc750829383c0a74372a9c2457e
Dimitri Roche <dimroc@gmail.com>
2017-09-08 19:27:31 -0400

    density_map can generate 3d np arrays

--- ml/crowdcount/models/density_map.py
+++ ml/crowdcount/models/density_map.py
@@ -15,6 +15,14 @@ def generate(image_path, annotations):
     return cv2.GaussianBlur(pixels, (_gaussian_kernel, _gaussian_kernel), 0)
 
 
+def generate_3d(image_path, annotations):
+    """
+    Add axis at the end to be compatible w keras.
+    e.g. [640, 480] becomes [640, 480, 1]
+    """
+    return generate(image_path, annotations)[..., None]
+
+
 def _sum_heads(pixels, annotations):
     for a in annotations:
         pixels[int(a[1]), int(a[0])] += 1
