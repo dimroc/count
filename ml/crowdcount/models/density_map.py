5121d83916dd67b8f52f9d7f5b6843b57e316ea1
Dimitri Roche <dimroc@gmail.com>
2017-09-09 10:21:48 -0400

    WIP: Trying to get the labels to match the size of the fcn's output

--- ml/crowdcount/models/density_map.py
+++ ml/crowdcount/models/density_map.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 _gaussian_kernel = 35
+_scale_for_fcn = 1 / 4.5859872611  # Hardcoded to match final dimensions of FCN
 
 
 def generate(image_path, annotations):
@@ -15,12 +16,17 @@ def generate(image_path, annotations):
     return cv2.GaussianBlur(pixels, (_gaussian_kernel, _gaussian_kernel), 0)
 
 
-def generate_3d(image_path, annotations):
+def generate_fcn(image_path, annotations):
     """
     Add axis at the end to be compatible w keras.
     e.g. [640, 480] becomes [640, 480, 1]
     """
-    return generate(image_path, annotations)[..., None]
+    return downsample(generate(image_path, annotations))[..., None]
+
+
+def downsample(array):
+    width, height, *_ = array.shape
+    return cv2.resize(array, (round(height * _scale_for_fcn), round(width * _scale_for_fcn)))
 
 
 def _sum_heads(pixels, annotations):
