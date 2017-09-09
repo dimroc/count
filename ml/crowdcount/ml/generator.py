9c288d66ac6a865267bd1c3bb228c0fe4731a1ed
Dimitri Roche <dimroc@gmail.com>
2017-09-09 16:21:09 -0400

    Clean up ml/generator.py

--- ml/crowdcount/ml/generator.py
+++ ml/crowdcount/ml/generator.py
@@ -9,20 +9,8 @@ import numpy as np
 _training_paths, _test_paths = groundtruth.train_test_split()
 
 
-def _load_features_labels(path):
-    # TODO: Clean up into a class and allow batch sizes.
-    x = kimg.img_to_array(kimg.load_img(path))
-    y = density_map.generate_3d(path, groundtruth.get(path))
-    batch_x = np.zeros((1,) + x.shape, dtype=K.floatx())
-    batch_y = np.zeros((1,) + y.shape, dtype=K.floatx())
-    batch_x[0], batch_y[0] = x, y
-    return batch_x, batch_y
-
-
 def training():
-    while True:
-        for path in sample(_training_paths, steps_per_epoch()):
-            yield _load_features_labels(path)
+    return features_and_labels_loop(_training_paths)
 
 
 def steps_per_epoch():
@@ -30,10 +18,28 @@ def steps_per_epoch():
 
 
 def validation():
-    while True:
-        for path in sample(_test_paths, validation_steps()):
-            yield _load_features_labels(path)
+    return features_and_labels_loop(_test_paths)
 
 
 def validation_steps():
     return len(_test_paths)
+
+
+def features_and_labels_loop(paths):
+    while True:
+        for path in sample(paths, len(paths)):
+            yield _load_features_labels(path)
+
+
+def _load_features_labels(path):
+    """
+    We must always have batch sizes of 1 because the images can be
+    of different dimensions, and numpy doesn't support arrays with
+    variable widths and heights.
+    """
+    x = kimg.img_to_array(kimg.load_img(path))
+    y = density_map.generate_3d(path, groundtruth.get(path))
+    batch_x = np.zeros((1,) + x.shape, dtype=K.floatx())
+    batch_y = np.zeros((1,) + y.shape, dtype=K.floatx())
+    batch_x[0], batch_y[0] = x, y
+    return batch_x, batch_y
