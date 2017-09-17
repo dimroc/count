38e3322e18215eeac9ffd0c84607f6ed596e40ed
Dimitri Roche <dimroc@gmail.com>
2017-09-17 13:35:38 -0400

    linecount mask is robust enough to handle input of varying dimensions

--- ml/crowdcount/ml/linecount/mask.py
+++ ml/crowdcount/ml/linecount/mask.py
@@ -1,9 +1,11 @@
 import cv2
+import numpy as np
 
 
 _mask = cv2.imread("data/shakecam_line_mask.png", 0) // 255
 
 
 def predict(x):
+    x = np.squeeze(x)
     assert _mask.shape == x.shape, "mask {} does not match input {}".format(_mask.shape, x.shape)
     return (_mask * x).sum()
