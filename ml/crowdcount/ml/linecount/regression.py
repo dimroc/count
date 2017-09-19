7d7195a52e81488e0b557bbdf6f2af4194a838a4
Dimitri Roche <dimroc@gmail.com>
2017-09-18 22:42:42 -0400

    Update model and optimizer

--- ml/crowdcount/ml/linecount/regression.py
+++ ml/crowdcount/ml/linecount/regression.py
@@ -29,7 +29,7 @@ class Model:
             self.initial_epoch = 0
 
         self.model.compile(loss='mean_squared_error',
-                optimizer=keras.optimizers.adam(lr=1e-6, decay=5e-5),
+                optimizer=keras.optimizers.adam(lr=1e-4, decay=5e-4),
                 metrics=['mse', 'mae', 'accuracy'])
 
     def predict(self, x):
@@ -40,7 +40,7 @@ class Model:
         self.model.fit_generator(generator.training(),
                 generator.steps_per_epoch(),
                 initial_epoch=self.initial_epoch,
-                epochs=100 - self.initial_epoch,
+                epochs=200 - self.initial_epoch,
                 verbose=1,
                 validation_data=generator.validation(),
                 validation_steps=generator.validation_steps(),
