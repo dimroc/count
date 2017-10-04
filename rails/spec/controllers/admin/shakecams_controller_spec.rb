ac56043dc913e72f629b621b7919a2ef5ad289cb
Dimitri Roche <dimroc@gmail.com>
2017-10-04 11:28:00 -0400

    Rename predictions routes and controllers to shakecams

--- rails/spec/controllers/admin/shakecams_controller_spec.rb
+++ rails/spec/controllers/admin/shakecams_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Admin::PredictionsController, type: :controller do
+RSpec.describe Admin::ShakecamsController, type: :controller do
   it_behaves_like "an admin controller"
 end
