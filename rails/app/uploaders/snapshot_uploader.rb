17141c312013441f517d4fe64a87e3169befaa79
Dimitri Roche <dimroc@gmail.com>
2017-10-04 07:24:49 -0400

    WIP: Improve naming for shrine files and tweak acl

--- rails/app/uploaders/snapshot_uploader.rb
+++ rails/app/uploaders/snapshot_uploader.rb
@@ -4,11 +4,10 @@ class SnapshotUploader < PredictionUploader
   include ImageProcessing::MiniMagick
   plugin :processing
   plugin :versions
-  plugin :upload_options, cache: { acl: "public-read" }
+  plugin :upload_options, store: { acl: "AllUsers:R" }
 
   process(:store) do |io, context|
     original = io.download
-    cropped = resize_to_fill!(original, 720, 720, gravity: "east")
-    {original: io, cropped: cropped}
+    {original: io, cropped: crop!(original, 720, 720, 0, 0)}
   end
 end
