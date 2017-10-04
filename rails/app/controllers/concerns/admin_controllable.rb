62b55aa13a80d3e69fd4b030a31ade7b47b3c37b
Dimitri Roche <dimroc@gmail.com>
2017-10-04 10:54:38 -0400

    redirect to correct page after login

--- rails/app/controllers/concerns/admin_controllable.rb
+++ rails/app/controllers/concerns/admin_controllable.rb
@@ -3,13 +3,17 @@ module AdminControllable
 
   included do
     layout 'admin'
-    before_action :authorize_admin
+    before_action :authenticate_user!, :authorize_admin
     after_action :verify_authorized
 
     private
 
     def authorize_admin
       authorize :admin, :granted?
+    end
+
+    def after_sign_in_path_for(user)
+      stored_location_for(:user) || super
     end
   end
 end
