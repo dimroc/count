c43afde7a09f91cc14dc26a2e73b8caf8828fab5
Dimitri Roche <dimroc@gmail.com>
2017-10-06 08:27:36 -0400

    Rename mldensity to train_density

--- ml/crowdcount/management/commands/train_density.py
+++ ml/crowdcount/management/commands/train_density.py
@@ -4,13 +4,11 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('--train', action='store_true', default=False)
         parser.add_argument('--weights', default=None)
         parser.add_argument('--test', action='store_true', default=False)
 
     def handle(self, *args, **kwargs):
-        if kwargs['train']:
-            density.train(kwargs['weights'])
-
         if kwargs['test']:
             density.test(None, kwargs['weights'])
+        else:
+            density.train(kwargs['weights'])
