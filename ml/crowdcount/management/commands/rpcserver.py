67239250504127e4dac8193ef4897a5f55dea74a
Dimitri Roche <dimroc@gmail.com>
2017-10-09 23:14:23 -0400

    [ml] ability to toggle which version to serve

--- ml/crowdcount/management/commands/rpcserver.py
+++ ml/crowdcount/management/commands/rpcserver.py
@@ -1,8 +1,14 @@
+from crowdcount.ml.predictor import DEFAULT_VERSION
 from django.core.management.base import BaseCommand
 import crowdcount.rpc.server as rpcserver
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('--mlversion', type=int, default=DEFAULT_VERSION)
+
     def handle(self, *args, **kwargs):
-        print("Starting ML gRPC server on port {}".format(rpcserver.DEFAULT_PORT))
-        rpcserver.serve()
+        version = kwargs['mlversion']
+        port = rpcserver.port_for(version)
+        print("Starting ML v{} gRPC server on port {}".format(version, port))
+        rpcserver.serve(version)
