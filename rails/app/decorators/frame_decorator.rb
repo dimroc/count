1d311f603b773fdf149ee759de9b475d66c4d9cd
Dimitri Roche <dimroc@gmail.com>
2017-10-12 10:52:56 -0400

    Allow user to click prediction thumbnail and see original

--- rails/app/decorators/frame_decorator.rb
+++ rails/app/decorators/frame_decorator.rb
@@ -2,7 +2,11 @@ class FrameDecorator < ApplicationDecorator
   delegate_all
 
   def image_tag
-    h.ix_image_tag(object.image.url, { sizes: "180px" }) if object.image
+    if object.image
+      h.link_to(object.image.url) do
+        h.ix_image_tag(object.image.url, { sizes: "180px" })
+      end
+    end
   end
 
   def created_at
