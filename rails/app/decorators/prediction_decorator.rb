e65c3a5bd8ab1f01775fd760a015c82443de79e4
Dimitri Roche <dimroc@gmail.com>
2017-10-12 10:52:56 -0400

    Allow user to click prediction thumbnail and see original

--- rails/app/decorators/prediction_decorator.rb
+++ rails/app/decorators/prediction_decorator.rb
@@ -2,7 +2,11 @@ class PredictionDecorator < ApplicationDecorator
   delegate_all
 
   def density_map_tag
-    h.ix_image_tag(object.density_map.url, { sizes: "180px" }) if object.density_map
+    if object.density_map
+      h.link_to(object.density_map.url) do
+        h.ix_image_tag(object.density_map.url, { sizes: "180px" })
+      end
+    end
   end
 
   def created_at
