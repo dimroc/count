90f369c4c043b569368d3167716f18933866ceb6
Dimitri Roche <dimroc@gmail.com>
2017-10-13 09:32:27 -0400

    Have to explicitly decorate frame because of STI and eager loading

--- rails/app/controllers/admin/frames_controller.rb
+++ rails/app/controllers/admin/frames_controller.rb
@@ -4,7 +4,9 @@ class Admin::FramesController < ApplicationController
   def update
     @frame = Frame.find(params[:id])
     @frame.update_attributes(flagged: !@frame.flagged)
-    reply = render_to_string partial: 'admin/shared/frame_row', locals: { frame: @frame.decorate }
+    reply = render_to_string(
+      partial: 'admin/shared/frame_row',
+      locals: { frame: FrameDecorator.decorate(@frame) })
     render json: { html: reply }
   end
 end
