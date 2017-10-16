db858695ea2cadd5570f9f679276648ab8171595
Dimitri Roche <dimroc@gmail.com>
2017-10-15 20:49:12 -0400

    Only show work hour range

--- rails/app/models/frame/shakecam.rb
+++ rails/app/models/frame/shakecam.rb
@@ -1,6 +1,9 @@
 class Frame::Shakecam < Frame
   include ShakecamUploader[:raw]
 
+  scope :day, -> (day) { where(created_at: working_hours(day)) }
+  scope :today, -> { where(created_at: working_hours(DateTime.now).begin..DateTime.now) }
+
   class << self
     def predict!
       now = DateTime.now.to_i
@@ -12,6 +15,11 @@ class Frame::Shakecam < Frame
     rescue StandardError, RuntimeError, Google::Apis::ServerError
       frame.destroy if frame.present? and !frame.destroyed?
       raise
+    end
+
+    def working_hours(day)
+      tomorrow = day + 1.day
+      day.in_time_zone(timezone).change(hour: 8, min: 0, sec: 0)..tomorrow.in_time_zone(timezone).end_of_day
     end
   end
 
