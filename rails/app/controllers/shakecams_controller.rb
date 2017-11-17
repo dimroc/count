eca5296fcdace19b0954f066e0104a88d5421389
Dimitri Roche <dimroc@gmail.com>
2017-11-17 09:15:19 -0500

    Remove shakecam stats since it’s no longer used

--- rails/app/controllers/shakecams_controller.rb
+++ rails/app/controllers/shakecams_controller.rb
@@ -7,8 +7,7 @@ class ShakecamsController < ApplicationController
         ActiveModelSerializers::SerializableResource.new(Frame::Shakecam.v2.day(2.days.ago)),
         ActiveModelSerializers::SerializableResource.new(Frame::Shakecam.v2.day(3.days.ago)),
         ActiveModelSerializers::SerializableResource.new(Frame::Shakecam.v2.day(4.days.ago)),
-      ],
-      stats: stats
+      ]
     }
   end
 
@@ -20,12 +19,5 @@ class ShakecamsController < ApplicationController
     else
       relation
     end
-  end
-
-  def stats
-    {
-      count: Frame::Shakecam.v2.count,
-      days: (Frame::Shakecam.v2.first.created_at.to_date - Frame::Shakecam.v2.last.created_at.to_date).to_i
-    }
   end
 end
