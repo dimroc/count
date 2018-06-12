713a96cc4dd1885f7437ec14172994c1380c3d09
Dimitri Roche <dimroc@gmail.com>
2018-06-12 12:05:52 -0400

    Add metadata to coreml model

--- ml/crowdcount/management/commands/convert_to_coreml.py
+++ ml/crowdcount/management/commands/convert_to_coreml.py
@@ -13,5 +13,15 @@ class Command(BaseCommand):
         path = ccp.weights_for("density", version)
         output = kwargs['output']
         print("Converting {0} to {1}".format(path, output))
-        coreml_model = coremltools.converters.keras.convert(path)
+        coreml_model = coremltools.converters.keras.convert(
+            path,
+            input_names='image',
+            output_names='density_map')
+
+        # Set model metadata
+        coreml_model.author = 'Dimitri Roche'
+        coreml_model.short_description = 'Generates a density map with crowd estimate.'
+        coreml_model.input_description['image'] = 'Image to calculate density map'
+        coreml_model.output_description['density_map'] = 'Density map where the sum of pixels is crowd count'
+
         coreml_model.save(output)
