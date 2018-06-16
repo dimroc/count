243b2bedc3d1aab2b890b50719511e27c30256e7
Dimitri Roche <dimroc@gmail.com>
2018-06-16 18:17:24 -0400

    Begin TDD of Core ML predictor to be used by platform channel

---
+++ flutter/ios/Runner/Predictor.swift
@@ -0,0 +1,17 @@
+//
+//  Predictor.swift
+//  Runner
+//
+//  Created by Dimitri Roche on 6/16/18.
+//  Copyright © 2018 The Chromium Authors. All rights reserved.
+//
+
+import Foundation
+
+class Predictor {
+    var model = crowdcount()
+
+    func predict(input: Int) -> Int {
+        return input
+    }
+}
