917e97877569ea6fd029511f89f677456aac41e3
Dimitri Roche <dimroc@gmail.com>
2018-07-05 14:04:09 -0400

    Update Prediction.playground to use xibs

--- ios/Prediction.playground/Contents.swift
+++ ios/Prediction.playground/Contents.swift
@@ -1,58 +1,34 @@
-// A Cocoa based Playground to drop images for crowd prediction
+//: A Cocoa based Playground to experiment with crowd prediction
 
 import AppKit
-import Cocoa
-import CrowdCountApi
 import PlaygroundSupport
 
-// Support Foundation calls on String
-public extension String { public var ns: NSString {return self as NSString} }
+let nibFile = NSNib.Name("MyView")
+var topLevelObjects : NSArray?
 
-/// Custom Labeled Playground-Based Drag-and-Drop window
-public class DropView: NSTextField {
-    
-    // Default action handler
-    public var handler: ([String]) -> Void = { paths in Swift.print(paths) }
-    
-    // Drag and drop notification
-    public override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation { return NSDragOperation.copy }
-    public override func draggingUpdated(_ sender: NSDraggingInfo) -> NSDragOperation { return NSDragOperation.copy }
-    public override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
-        let pboard = sender.draggingPasteboard
-        guard let paths = pboard.propertyList(forType: NSPasteboard.PasteboardType("NSFilenamesPboardType")) as? [String] else { return false }
-        handler(paths)
-        return true
+Bundle.main.loadNibNamed(nibFile, owner:nil, topLevelObjects: &topLevelObjects)
+
+let views = (topLevelObjects as! Array<Any>).filter { $0 is NSView }
+let topView = views[0] as! NSView
+let imageWell = topView.subviews[0].subviews[1] as! NSImageView // Hardcoded to match MyView.xib
+imageWell.isEditable = true
+
+typealias ObserverCallback = (NSImage) -> Void
+
+class ImageObserver: NSObject {
+    var callback: ObserverCallback    
+    init(_ callback: @escaping ObserverCallback) {
+        self.callback = callback
     }
-    
-    public required init?(coder: NSCoder) { super.init(coder: coder) }
-    public override init(frame frameRect: NSRect) {
-        super.init(frame: frameRect)
-        
-        // Presentation
-        stringValue = "Drop Here"; isEditable = false
-        font = NSFont(name: "Palatino", size: 48.0); alignment = .center
-        wantsLayer = true; layer?.backgroundColor = NSColor.white.cgColor
-        
-        // Register for file name drags
-        registerForDraggedTypes([NSPasteboard.PasteboardType(rawValue: kUTTypeFileURL as String), NSPasteboard.PasteboardType(rawValue: kUTTypeItem as String)])
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        let newImage = change![NSKeyValueChangeKey.newKey] as! NSImage
+        self.callback(newImage)
     }
 }
 
-// Establish drop view
-let dropView = DropView(frame: NSRect(x: 0, y: 0, width: 600, height: 200))
-dropView.stringValue = "Drop Pictures"
-dropView.handler = {
-    // Print out each file name and image size
-    $0.forEach {
-        if let image = NSImage(contentsOfFile: $0) {
-            Swift.print($0.ns.lastPathComponent, image.size)
-        } else {
-            Swift.print($0.ns.lastPathComponent, "is not an image")
-        }
-    }
-}
+let observer = ImageObserver({ image in Swift.print(image) })
+imageWell.addObserver(observer, forKeyPath: "image", options: NSKeyValueObservingOptions.new, context: nil)
 
 // Present the view in Playground
-PlaygroundPage.current.liveView = dropView
-PlaygroundPage.current.needsIndefiniteExecution = true
-
+PlaygroundPage.current.liveView = topView
