374ad582e8f6dfe50edc9c7cdba43385595d59ff
Dimitri Roche <dimroc@gmail.com>
2018-07-21 08:31:36 -0400

    Simplify face detection callback

--- ios/CrowdCountApi/FaceDetector.swift
+++ ios/CrowdCountApi/FaceDetector.swift
@@ -12,23 +12,13 @@ import Promises
 
 class FaceDetector {
     public static func detect(within cgimage: CGImage) -> [CGRect] {
-        var boundingBoxes: [CGRect]?
-        let request = VNDetectFaceRectanglesRequest { request, err in
-            guard err == nil else {
-                print(err!)
-                return
-            }
-            
-            guard let results = request.results else {
-                return
-            }
-            
-            let faces: [VNFaceObservation] = results.filter { $0 is VNFaceObservation } as! [VNFaceObservation]
-            boundingBoxes = faces.map { $0.boundingBox }
-        }
-        
+        let request = VNDetectFaceRectanglesRequest()
         let handler = VNImageRequestHandler(cgImage: cgimage, options: [:])
         try! handler.perform([request])
-        return boundingBoxes!
+        guard let results = request.results else {
+            return []
+        }
+        let faces: [VNFaceObservation] = results.filter { $0 is VNFaceObservation } as! [VNFaceObservation]
+        return faces.map { $0.boundingBox }
     }
 }
