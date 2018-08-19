3531bc871201b459b4a984d7ab364ff5cf1dc1f4
Dimitri Roche <dimroc@gmail.com>
2018-08-19 14:46:47 -0400

    Have macOS app show prediction grid to the right

--- ios/CrowdCountMac/DragDropViewController.swift
+++ ios/CrowdCountMac/DragDropViewController.swift
@@ -14,12 +14,16 @@ import Promises
 // Ported from the Prediction.playground because compilation and linking
 // of playgrounds was just too flaky. "error: Couldn't lookup symbols", etc
 class DragDropViewController: NSViewController {
-    @IBOutlet weak var stackView: NSStackView!
+    @IBOutlet weak var scrollView: NSScrollView!
+    @IBOutlet weak var horizontalStackView: NSStackView!
+    @IBOutlet weak var verticalStackView: NSStackView!
     @IBOutlet weak var imageWell: NSImageView!
     @IBOutlet weak var predictionLabel: NSTextField!
     
     let predictor = FriendlyPredictor()
-    var predictionGrid: NSGridView?, classificationGrid: NSGridView?
+    var predictionGrid: NSGridView?
+    var classificationGrid: NSGridView?
+    let scrollViewGroup = ConstraintGroup()
     
     override func viewDidAppear() {
         super.viewDidAppear()
@@ -28,8 +32,8 @@ class DragDropViewController: NSViewController {
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         predictionLabel.stringValue = "Predicting..."
-        removeFromPlayground(predictionGrid)
-        removeFromPlayground(classificationGrid)
+        removeFromPlayground(predictionGrid, stackView: horizontalStackView)
+        removeFromPlayground(classificationGrid, stackView: verticalStackView)
         
         let image = change![NSKeyValueChangeKey.newKey] as! NSImage
         all(
@@ -65,30 +69,36 @@ class DragDropViewController: NSViewController {
         gridView.setContentHuggingPriority(NSLayoutConstraint.Priority(600), for: .horizontal)
         gridView.setContentHuggingPriority(NSLayoutConstraint.Priority(600), for: .vertical)
         
-        stackView.addArrangedSubview(gridView)
+        horizontalStackView.addArrangedSubview(gridView)
         constrain(gridView) { gv in
-            gv.height >= 600
+            gv.top == gv.superview!.top + 40
+            gv.bottom == gv.superview!.bottom - 40
+        }
+        
+        constrain(scrollView, replace: scrollViewGroup) { scroll in
+            scroll.width == CGFloat(1000)
+            scroll.height == CGFloat(700)
         }
         return gridView
     }
     
     private func generateClassificationGrid(_ classification: FriendlyClassification) -> NSGridView {
-        predictionLabel.stringValue = "Winning Classification: " + classification.classification + "(scroll down)"
+        predictionLabel.stringValue = "Winning Classification: " + classification.classification
         print("Probabilities", classification.probabilities)
         let labels: [[NSView]] = classification.probabilities.map { tuple -> [NSView] in
             let (key, value) = tuple
-            return [NSTextField(labelWithString: key), NSTextField(labelWithString: String.init(reflecting: value))]
+            return [NSTextField(labelWithString: key), NSTextField(labelWithString: String(format: "%.4f%%", value * 100))]
         }
         let gridView = NSGridView(views: labels)
         gridView.translatesAutoresizingMaskIntoConstraints = false
         gridView.setContentHuggingPriority(NSLayoutConstraint.Priority(600), for: .horizontal)
         gridView.setContentHuggingPriority(NSLayoutConstraint.Priority(600), for: .vertical)
         
-        stackView.addArrangedSubview(gridView)
+        verticalStackView.addArrangedSubview(gridView)
         return gridView
     }
     
-    private func removeFromPlayground(_ view: NSView?) {
+    private func removeFromPlayground(_ view: NSView?, stackView: NSStackView) {
         if view != nil {
             stackView.removeArrangedSubview(view!)
             view!.removeFromSuperview()
